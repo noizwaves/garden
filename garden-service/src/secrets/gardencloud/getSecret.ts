9d125c7bb9c2a9ee977f6b84854bd21d8d4d88dc
Emanuele Libralato <e.libralato@gmail.com>
2020-02-19 23:03:48 +0100

    feat: added env for hostname and auth

--- garden-service/src/secrets/gardencloud/getSecret.ts
+++ garden-service/src/secrets/gardencloud/getSecret.ts
@@ -1,5 +1,7 @@
 import request = require('request-promise-native');
-const GARDEN_CLOUD_URL = "http://solomon-cloud-api.cloud.dev.garden.io/secrets/project"
+const GARDEN_CLOUD_HOSTNAME = process.env.GARDEN_CLOUD_URL || "https://cloud.garden.io"
+// const GARDEN_CLOUD_VERSION = "v1"
+const CLI_TOKEN = "THE_CLI_AUTH_TOKEN"
 
 export async function getSecretsFromGardenCloud(project: string, env: string) {
     const response = await request({
@@ -7,8 +9,11 @@ export async function getSecretsFromGardenCloud(project: string, env: string) {
         resolveWithFullResponse: true,
         simple: false,
         strictSSL: false,
-        uri: `${GARDEN_CLOUD_URL}/1/env/${env}`,
-        method: "GET"
+        uri: `${GARDEN_CLOUD_HOSTNAME}/secrets/project/1/env/${env}`,
+        method: "GET",
+        qs: {
+          cli_token: CLI_TOKEN,
+        }
     })
 
     if (response && response.statusCode === 200 && response.body && response.body.status === "success") {
